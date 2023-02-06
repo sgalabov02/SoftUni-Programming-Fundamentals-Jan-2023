55712505b3210d8e45e07a94a6a9ed7b56eb50c3
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-06 15:31:11 +0200

    Create ConcatNames_05.java

---
+++ 4. Data Types and Variables - Lab/ConcatNames_05.java
@@ -0,0 +1,16 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ConcatNames_05 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		String firstName = scanner.nextLine();
+		String lastName = scanner.nextLine();
+		String delimiter = scanner.nextLine();
+		
+		System.out.println(firstName + delimiter + lastName);
+	}
+
+}
