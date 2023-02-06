9808409f88ca2e228a3a1e89dc71104408b7692e
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-06 15:44:37 +0200

    Create RefactorSpecialNumbers_12.java

---
+++ 4. Data Types and Variables - Lab/RefactorSpecialNumbers_12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RefactorSpecialNumbers_12 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int nums = Integer.parseInt(scanner.nextLine());
+		int sum = 0;
+		for (int i = 1; i <= nums; i++) {
+		    int temp = i;
+		    while (i > 0) {
+		        sum += i % 10;
+		        i = i / 10;
+		    }
+		    if(sum == 5 || sum == 7 || sum == 11) {
+		    	System.out.printf("%d -> True\n", temp);
+		    } else {
+		    	System.out.printf("%d -> False\n", temp);
+		    }
+		    sum = 0;
+		    i = temp;
+		}
+	}
+
+}
