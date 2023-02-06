b56d11da71a676b04bdc18369b62a61c1de95dca
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-06 23:31:48 +0200

    Update SumEvenNumbers_03.java

--- 7. Arrays - Lab/SumEvenNumbers_03.java
+++ 7. Arrays - Lab/SumEvenNumbers_03.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main2 {
+public class SumEvenNumbers_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
