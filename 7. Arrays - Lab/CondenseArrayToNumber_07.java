222c8fb2d5b49d35252454224b796c49e0dc551a
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-06 23:34:55 +0200

    Create CondenseArrayToNumber_07.java

---
+++ 7. Arrays - Lab/CondenseArrayToNumber_07.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CondenseArrayToNumber_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        int[] condensed = new int[numbers.length - 1];
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers.length == 1) {
+                break;
+            }
+            if (i == numbers.length - 1) {
+                int[] newCondensed = new int[condensed.length - 1];
+                i = -1;
+                numbers = condensed;
+                condensed = newCondensed;
+            }
+            else {
+                condensed[i] = numbers[i] + numbers[i + 1];
+            }
+        }
+
+        System.out.println(numbers[0]);
+    }
+}
