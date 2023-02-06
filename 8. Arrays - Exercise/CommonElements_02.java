264aba1e17f4c2673abd988dc736367884b6f424
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-06 23:38:59 +0200

    Create CommonElements_02.java

---
+++ 8. Arrays - Exercise/CommonElements_02.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CommonElements_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] arr1 = scanner.nextLine().split(" ");
+        String[] arr2 = scanner.nextLine().split(" ");
+
+        for(int i = 0; i < arr2.length; i++) {
+            for(int j = 0; j < arr1.length; j++) {
+                if(arr2[i].equals(arr1[j])) {
+                    System.out.print(arr2[i] + " ");
+                }
+            }
+        }
+
+    }
+}
