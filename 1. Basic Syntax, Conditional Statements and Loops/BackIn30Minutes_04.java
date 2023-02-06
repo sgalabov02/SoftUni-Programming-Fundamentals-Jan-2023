aafe1d8ade4b00151b366d24a3e600f7da38b47e
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-06 13:39:36 +0200

    Create BackIn30Minutes_04.java

---
+++ 1. Basic Syntax, Conditional Statements and Loops/BackIn30Minutes_04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int hours = Integer.parseInt(scanner.nextLine());
+		int minutes = Integer.parseInt(scanner.nextLine());
+		
+		int totalTime = (hours * 60) + minutes + 30;
+		
+		hours = totalTime / 60;
+		minutes = totalTime % 60;
+		
+		if(hours > 23) {
+			hours = 0;
+		}
+		System.out.printf("%d:%02d", hours, minutes);
+		
+	}
+
+}
