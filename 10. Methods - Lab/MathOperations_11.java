6ecea8ed8bd33b082a4546f00c0daf4c7fa2515a
sgalabov02 <91763598+sgalabov02@users.noreply.github.com>
2023-02-07 07:00:10 +0200

    Create MathOperations_11.java

---
+++ 10. Methods - Lab/MathOperations_11.java
@@ -0,0 +1,35 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class MathOperations_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = Integer.parseInt(scanner.nextLine());
+        String operator = scanner.nextLine();
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(mathOperation(operator, num1, num2));
+
+    }
+
+    public static int mathOperation(String operator, int num1, int num2) {
+        int result = 0;
+        switch(operator) {
+            case "/":
+                result = num1 / num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+        }
+        return result;
+    }
+
+}
